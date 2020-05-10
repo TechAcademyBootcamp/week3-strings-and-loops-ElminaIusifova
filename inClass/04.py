e8e02a0562f298162684ecde551ce8f4bec18e5d
ElminaIusifova <32398063+ElminaIusifova@users.noreply.github.com>
2020-05-10 00:30:55 -0400

    taks3

--- inClass/04.py
+++ inClass/04.py
@@ -1,1 +1,11 @@
 # Task3(Hard). Write a Python program to remove duplicates from a list.
+
+list = [4, 5, 6, 3, 1, 4, 5, 6, 7, 4, 6]
+print(f"Before dublicates: {list}")
+
+array = []
+
+for i in list:
+    if i not in array:
+        array.append(i)
+print(f"After dublicates: {array}")
